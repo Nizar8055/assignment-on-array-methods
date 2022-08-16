eabe0ddae36a7eae7b5fdfd1a83cc0d6c5a17c50
Nizar8055 <104364273+Nizar8055@users.noreply.github.com>
2022-08-16 22:23:09 +0530

    Add files via upload

---
+++ assignment on array methods/script.js
@@ -0,0 +1,36 @@
+// Question number 1
+// document.write('welcome to javascript')
+// alert('welcome to Javascript')
+
+// console.log(alert)
+
+//Question number 2 External javascript
+// alert('This is from external javascript')
+
+//Question number 3
+
+// const user= {
+//   userId:'12345',
+//   firstName:'Nizar',
+//   lastName:'KM',
+//   password:'1234567',
+//   email:'nizzunizar321@gmail.com',
+//   gender:'male',
+//   age:23,
+// }
+// alert(user)
+
+//Question number 4 
+
+// let laptop ={
+//   cpu: '16',
+//   storage:'24gb',
+//   brand:'lenova',
+//   processor:'ryzen',
+//   battery:'5000watt'
+// }
+
+// console.log(laptop)
+
+//Question number 5
+
